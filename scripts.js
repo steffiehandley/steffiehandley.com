16636cc88bbe406fd525309735e0cc97d178c1e6
Stephanie Handley <steffiehandley@gmail.com>
2020-02-09 20:39:11 -0500

    links

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var popup = document.querySelector(".popup");
+        var toggle = document.querySelector(".toggle")
+        
+        function togpop() {
+            popup.style.display = "block"
+        }
+
+        toggle.addEventListener("click",togpop)
